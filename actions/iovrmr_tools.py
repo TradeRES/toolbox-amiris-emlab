59ba1691bc12c74d5b3b8ec67fa5e8c742aebe9a
Johannes Kochems <johannes.kochems@dlr.de>
2022-05-17 15:39:17 +0200

    integrate storages and renewables into workflow

--- actions/iovrmr_tools.py
+++ actions/iovrmr_tools.py
@@ -1,23 +1,30 @@
 # -*- coding:utf-8 -*-
 
-__version__ = '0.2.0'
-__authors__ = 'Felix Nitsch'
-__maintainer__ = 'Felix Nitsch'
-__email__ = 'felix.nitsch@dlr.de'
+__version__ = "0.2.0"
+__authors__ = "Felix Nitsch"
+__maintainer__ = "Felix Nitsch"
+__email__ = "felix.nitsch@dlr.de"
 
 
 import os
-from enum import Enum
 import re
+from enum import Enum, auto
+from typing import List, Dict, Callable
 
-import yaml
 import numpy as np
 import pandas as pd
-
+import yaml
 from ioproc.logger import mainlogger
-from typing import List
 
 OUTPUT_FILE_ENDING = ".csv"
+OPERATOR_AGENTS = [
+    "VariableRenewableOperator",
+    "ConventionalPlantOperator",
+    "Biogas",
+    "StorageTrader",
+]
+SUPPORTED_AGENTS = ["RenewableTrader", "SystemOperatorTrader"]
+EXCHANGE = ["EnergyExchange"]
 
 
 class FilterType(Enum):
@@ -27,16 +34,24 @@ class FilterType(Enum):
     LESS = 3
     LESSEQUAL = 4
     NOT = 5
-    
-    
+
+
+class AmirisOutputs(Enum):
+    ID = auto()
+    REVENUES_IN_EURO = auto()
+    VARIABLE_COSTS_IN_EURO = auto()
+    CONTRIBUTION_MARGIN_IN_EURO = auto()
+    PRODUCTION_IN_MWH = auto()
+
+
 def raise_and_log_critical_error(error_message):
-    """ Raises a critical error and logs with given `error_message` """
+    """Raises a critical error and logs with given `error_message`"""
     mainlogger.critical(error_message)
     raise Exception(error_message)
 
 
 def to_list(data):
-    """ Ensures that `data` is list """
+    """Ensures that `data` is list"""
     if isinstance(data, list):
         return data
     elif isinstance(data, tuple):
@@ -51,15 +66,16 @@ class Header(Enum):
 
 
 def get_header(string):
-    """" Returns converted operator from given bool """
-    header_map = {True: Header.TRUE.value,
-                  False: Header.FALSE.value,
-                  }
+    """ " Returns converted operator from given bool"""
+    header_map = {
+        True: Header.TRUE.value,
+        False: Header.FALSE.value,
+    }
     return header_map[string]
 
 
 def get_field(dictionary, key):
-    """ Returns value from given `dictionary` and raises `KeyError` if `key` is not specified """
+    """Returns value from given `dictionary` and raises `KeyError` if `key` is not specified"""
     try:
         return dictionary[key]
     except KeyError:
@@ -68,9 +84,9 @@ def get_field(dictionary, key):
 
 
 def get_excel_sheet(params):
-    """ Returns a list of excel_sheets to parse from given dictionary or raises error when field is not specified """
+    """Returns a list of excel_sheets to parse from given dictionary or raises error when field is not specified"""
     try:
-        return get_field(params, 'excelSheet')
+        return get_field(params, "excelSheet")
     except KeyError:
         message = "Please specify the Excel sheet(s) to parse in a list using under the field `excelSheets`."
         raise_and_log_critical_error(message)
@@ -124,7 +140,7 @@ def determine_intersection_mask(data: pd.DataFrame, column_names: list, values_t
     ensure_all_str(values_to_filter)
     values_to_filter = get_at_least_2d_array(to_list(values_to_filter))
     for column_index, column_name in enumerate(column_names):
-        relevant_values = np.squeeze(values_to_filter[:, column_index:column_index + 1])
+        relevant_values = np.squeeze(values_to_filter[:, column_index : column_index + 1])
         if str(relevant_values) == "*":
             mask[column_index] = np.full(len(data[column_name]), True)
         else:
@@ -135,13 +151,13 @@ def determine_intersection_mask(data: pd.DataFrame, column_names: list, values_t
                 mask[column_index] = data[column_name].isin(relevant_values)
             else:
                 relevant_values = [str(entry).replace("*", ".*") for entry in relevant_values]
-                reg_ex_mask = "(" + '|'.join(relevant_values) + ")"
+                reg_ex_mask = "(" + "|".join(relevant_values) + ")"
                 mask[column_index] = apply_re_fullmatch(reg_ex_mask, data[column_name])
-    return mask.all(axis='columns')
+    return mask.all(axis="columns")
 
 
 def check_if_in_list(value: str, data: list):
-    """ Raises error if given 'value' is not part in 'data' while accounting for regex wildcard '*' """
+    """Raises error if given 'value' is not part in 'data' while accounting for regex wildcard '*'"""
     if not data:
         raise_and_log_critical_error("Cannot validate if `{}` is in list because of empty list.".format(value))
 
@@ -155,7 +171,7 @@ def check_if_in_list(value: str, data: list):
 
 
 def get_at_least_2d_array(data):
-    """ Returns at least 2 dimensional np array of given data """
+    """Returns at least 2 dimensional np array of given data"""
     data = np.array(data)
     if data.ndim < 2:
         data = np.expand_dims(data, axis=1)
@@ -163,7 +179,7 @@ def get_at_least_2d_array(data):
 
 
 def ensure_all_str(items):
-    """ Raises exception if any item in 'items' (flat or nested) is not of type string """
+    """Raises exception if any item in 'items' (flat or nested) is not of type string"""
     items = to_list(items)
     for item in items:
         if isinstance(item, list):
@@ -173,13 +189,13 @@ def ensure_all_str(items):
 
 
 def apply_re_fullmatch(mask, data):
-    """ Returns bool list of re.fullmatch when applying 'mask' on 'data' """
+    """Returns bool list of re.fullmatch when applying 'mask' on 'data'"""
     return [bool(re.fullmatch(mask, str(s))) for s in data]
 
 
 def write_yaml(config_file, output_file_path):
-    """ Writes given `config_file` in yaml format to `output_file_path` """
-    with open(output_file_path, 'w') as stream:
+    """Writes given `config_file` in yaml format to `output_file_path`"""
+    with open(output_file_path, "w") as stream:
         try:
             yaml.safe_dump(config_file, stream)
         except yaml.YAMLError:
@@ -187,13 +203,13 @@ def write_yaml(config_file, output_file_path):
 
 
 def ensure_path_exists(path):
-    """ Creates a specified path if not already existent """
+    """Creates a specified path if not already existent"""
     if not os.path.exists(path):
         os.makedirs(path)
 
 
 def unflatten_list_of_dicts(list_of_dicts):
-    """ Returns list of `input` dicts as unflattened dictionaries for keys with the special character `/` """
+    """Returns list of `input` dicts as unflattened dictionaries for keys with the special character `/`"""
     result = []
     for single_dict in list_of_dicts:
         result_dict = {}
@@ -225,10 +241,10 @@ def insert_agents_from_map(data, translation_map, template):
         registered_agents_per_plant = {}
         for translation in translation_map:
             if isinstance(translation, dict):
-                column = get_field(translation, 'column')
-                target = get_field(translation, 'target')
+                column = get_field(translation, "column")
+                target = get_field(translation, "target")
                 if row[column] == target:
-                    agent = create_agent(row, translation['map'])
+                    agent = create_agent(row, translation["map"])
                     agent_list.append(agent)
                 else:
                     continue
@@ -237,31 +253,31 @@ def insert_agents_from_map(data, translation_map, template):
                 agent_list.append(agent)
             else:
                 raise_and_log_critical_error("Failed creating agent specified as {}".format(translation))
-            registered_agents_per_plant[agent['Type']] = agent['Id']
+            registered_agents_per_plant[agent["Type"]] = agent["Id"]
         all_registered_agents.append(registered_agents_per_plant)
 
     agent_list = unflatten_list_of_dicts(agent_list)
 
-    if not template['Agents']:
-        template['Agents'] = agent_list
+    if not template["Agents"]:
+        template["Agents"] = agent_list
     else:
-        template['Agents'].extend(agent_list)
+        template["Agents"].extend(agent_list)
 
     return template, all_registered_agents
 
 
 def create_agent(row, translation):
-    """ Returns created agent from data in `row` according to specifications in `translation`. """
+    """Returns created agent from data in `row` according to specifications in `translation`."""
     agent = {}
     for item in translation:
-        field = get_field(item, 'attribute')
-        if 'value' in item:
-            value = get_field(item, 'value')
-        elif 'column' in item:
-            column = get_field(item, 'column')
+        field = get_field(item, "attribute")
+        if "value" in item:
+            value = get_field(item, "value")
+        elif "column" in item:
+            column = get_field(item, "column")
             value = get_field(row, column)
-            if 'suffix' in item:
-                value = str(value) + str(get_field(item, 'suffix'))
+            if "suffix" in item:
+                value = str(value) + str(get_field(item, "suffix"))
                 value = int(value)
         else:
             raise_and_log_critical_error("No 'value' or `column` found for attribute '{}'".format(item))
@@ -276,8 +292,8 @@ def get_filter_type_lists(filters):
     """
     filter_type_lists = []
     for filter_item in filters:
-        if 'type' in filter_item.keys():
-            filter_type = get_filter_type(filter_item['type'])
+        if "type" in filter_item.keys():
+            filter_type = get_filter_type(filter_item["type"])
         else:
             filter_type = FilterType.EQUAL
         filter_type_lists.append(filter_type)
@@ -285,12 +301,13 @@ def get_filter_type_lists(filters):
 
 
 def get_filter_type(filter_name):
-    """ Returns FilterType enum of given `filter_name` """
+    """Returns FilterType enum of given `filter_name`"""
     try:
         return FilterType[filter_name.upper()]
     except KeyError:
-        message = "Unknown filter type '{}'. Please provide one of the following: {}".\
-            format(filter_name, [f.name for f in FilterType])
+        message = "Unknown filter type '{}'. Please provide one of the following: {}".format(
+            filter_name, [f.name for f in FilterType]
+        )
         raise_and_log_critical_error(message)
 
 
@@ -299,10 +316,10 @@ def get_id_or_derive_from_type(contract, unit, param):
     Returns Id derived from `param`+`id` in given `contract` dict or derived from `param`+`Type` in unit dictionary.
     Raises error if Id cannot be found either in `contract` or `unit`.
     """
-    if param + 'Id' in contract:
-        return get_field(contract, param + 'Id')
+    if param + "Id" in contract:
+        return get_field(contract, param + "Id")
     else:
-        return unit[get_field(contract, param + 'Type')]
+        return unit[get_field(contract, param + "Type")]
     raise_and_log_critical_error("Cannot derive Id for the contract '{}'.".format(contract))
 
 
@@ -317,21 +334,21 @@ def insert_contracts_from_map(data, translation_map, template):
         for translation in translation_map:
             contract = {}
             for field in translation:
-                if 'sender' in field.lower():
-                    value = get_id_or_derive_from_type(translation, unit, 'Sender')
-                    field = 'SenderId'
-                elif 'receiver' in field.lower():
-                    value = get_id_or_derive_from_type(translation, unit, 'Receiver')
-                    field = 'ReceiverId'
+                if "sender" in field.lower():
+                    value = get_id_or_derive_from_type(translation, unit, "Sender")
+                    field = "SenderId"
+                elif "receiver" in field.lower():
+                    value = get_id_or_derive_from_type(translation, unit, "Receiver")
+                    field = "ReceiverId"
                 else:
                     value = get_field(translation, field)
                 contract.update({field: value})
             contract_list.append(contract)
 
-    if not template['Contracts']:
-        template['Contracts'] = contract_list
+    if not template["Contracts"]:
+        template["Contracts"] = contract_list
     else:
-        template['Contracts'].extend(contract_list)
+        template["Contracts"].extend(contract_list)
 
     return template
 
@@ -344,8 +361,9 @@ def round_to_full_hour(time_step: int, base=3600) -> int:
 def ensure_given_data_matches_dims(x_dim: int, y_dim: int, data: list):
     """Raises error when dimensions `x_dim` x `y_dim` don't match length of specified data"""
     if x_dim * y_dim != len(data):
-        raise_and_log_critical_error("Defined dimensions {}X{} don't match length of specified data {}".
-                                     format(x_dim, y_dim, len(data)))
+        raise_and_log_critical_error(
+            "Defined dimensions {}X{} don't match length of specified data {}".format(x_dim, y_dim, len(data))
+        )
 
 
 def check_if_window_too_large(win_size, simulation_length):
@@ -354,7 +372,9 @@ def check_if_window_too_large(win_size, simulation_length):
         difference = win_size - simulation_length
         difference = round(divmod(difference.total_seconds(), 3600)[0])
         msg = "Given window size is `{}` hours greater than simulation length. \
-            Please choose a shorter window size.".format(difference)
+            Please choose a shorter window size.".format(
+            difference
+        )
         mainlogger.warning(msg)
 
 
@@ -366,3 +386,26 @@ def get_all_files_in_folder_ending_with(folder: str, ending: str) -> List[str]:
 def get_all_csv_files_in_folder(folder: str) -> List[str]:
     """Returns list of strings including path to folder for CSV files"""
     return get_all_files_in_folder_ending_with(folder=folder, ending=OUTPUT_FILE_ENDING)
+
+
+def call_function_per_agent(type_df: pd.DataFrame, function: Callable[[pd.DataFrame], float]) -> Dict:
+    """Splits given dataframe by AgentId and applies given `function`; Returns dict of {AgentId: `function` return value}"""
+    groups = type_df.groupby(by="AgentId", sort=False)
+    return {agent_id: function(agent_df) for agent_id, agent_df in groups}
+
+
+def sum_per_agent(df: pd.DataFrame, columns: List[str]) -> pd.DataFrame:
+    """Calculates summed values per agent for each given column individually"""
+    all_values_per_agent = pd.DataFrame(columns=columns)
+    for column in columns:
+        function = calc_sum(column)
+        value_per_agent = call_function_per_agent(df, function)
+        for agent_id, value in value_per_agent.items():
+            all_values_per_agent.at[agent_id, column] = value
+
+    return all_values_per_agent
+
+
+def calc_sum(column: str) -> Callable[[pd.DataFrame], float]:
+    """Returns function on dataframe to calculate `sum of column`"""
+    return lambda df: df[column].sum()
