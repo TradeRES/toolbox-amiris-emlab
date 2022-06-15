976addad47040d2ec261c1bb6726435758b41c1e
IngridJSJ <jeenesan@gmail.com>
2022-06-15 17:51:27 +0200

    added grouped power plants to workflow

--- prepare_power_plants.py
+++ prepare_power_plants.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 """
 This script groups the power plants per age. 
 The capacity is summed and the efficiency is weighted averaged
+Plants smaller than X MW are grouped with the power plants of the nearest age
+Sanchez 15-06-22
 """
 country = "DE"
 file = "data/dutchGermanPlants2022_from_emla.xlsx"
@@ -11,17 +13,20 @@ dataframe = pd.read_excel(file)
 dataframe.loc[dataframe['Location'] == country]
 min_capacity_to_group = 50
 
-
+# Group power plants by age and efficiency
+dataframe.rename(columns={"Technology traderes": "Technology"}, inplace=True)
 dataframe.sort_values(by=['Age'], ascending=True)
 techs = dataframe["Technology"].unique()
 weighted_eff = lambda x: np.average(x, weights=dataframe.loc[x.index, "Capacity"])
 df = dataframe.groupby(["Technology", 'Age'], as_index=False).agg(total_capacity=("Capacity", "sum"),
                                                                   efficiency_weighted_mean=("Efficiency", weighted_eff))
 
+
 def weighted_avg(values, weights):
    return np.average(values, weights=weights)
 
 
+# Group power plants by age and efficiency. Plants smaller than 50 MW are grouped with the power plants of the nearest age
 alltechs = []
 for t in techs:
     small = df.loc[(df['total_capacity'] < min_capacity_to_group) & (df['Technology'] == t)]
@@ -37,8 +42,14 @@ for t in techs:
     alltechs.append(big)
 
 final = pd.concat(alltechs, ignore_index=True)
+final.rename(columns={"total_capacity": "Capacity", "efficiency_weighted_mean": "Efficiency"} , inplace=True , errors="raise")
+final['Location'] = country
+final['Owner'] = "Producer1"
+final['DischarginEfficiency'] = 0
+
 boxplot = final.boxplot(column=['Age'], by="Technology", rot=90, fontsize=8)
-boxplot_capacity = final.boxplot(column=['total_capacity'], by="Technology", rot=90, fontsize=8)
+boxplot_capacity = final.boxplot(column=['Capacity'], by="Technology", rot=90, fontsize=8)
+
 boxplot.plot()
 boxplot_capacity.plot()
 plt.show()
