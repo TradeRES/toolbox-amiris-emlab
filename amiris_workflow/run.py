f30747eb301545a0ae7efd27bff2a240403e485e
Christoph Schimeczek <Christoph.Schimeczek@dlr.de>
2022-07-19 17:29:24 +0200

    remove user-dependent configuration

--- amiris_workflow/run.py
+++ amiris_workflow/run.py
@@ -1,14 +1,8 @@
-#-*- coding:utf-8 -*-
-
-__version__ = '0.2.0'
-__authors__ = 'Felix Nitsch'
-__maintainer__ = 'Felix Nitsch'
-__email__ = 'felix.nitsch@dlr.de'
-
 import sys
 import os
+
 print("current", os.getcwd())
-import ioproc.runners as run
+import ioproc.runners as run  # noqa
 
 sys.path.append("../../")
 run.start()
--- emlabpy/util/clock.py
+++ emlabpy/util/clock.py
@@ -12,84 +12,108 @@ from util.spinedb import SpineDB
 from util import globalNames
 import csv
 import os
-db_url = sys.argv[1]
-db_emlab = SpineDB(db_url)
-
 
 
 def reset_candidate_investable_status():
     class_name = "CandidatePowerPlants"
-    candidate_powerplants = [i for i in db_emlab.query_object_parameter_values_by_object_class(class_name)]
+    candidate_powerplants = [i for i in db.query_object_parameter_values_by_object_class(class_name)]
     for candidate in candidate_powerplants:
-        db_emlab.import_object_parameter_values([(class_name, candidate["object_name"] , "ViableInvestment",  bool(1) , '0')])
+        db.import_object_parameter_values([(class_name, candidate["object_name"], "ViableInvestment", bool(1), "0")])
 
-def update_years_file(current_year , lookAhead, final_year):
-    fieldnames = ['current', 'future', "final"]
+
+def update_years_file(current_year, lookAhead, final_year):
+    fieldnames = ["current", "future", "final"]
     print("updated years file")
-    #TODO dont hard code the path, once the spine bug is fixed. then it can be exported to the output folder.
+    # TODO dont hard code the path, once the spine bug is fixed. then it can be exported to the output folder.
     # the clock is executed in the util folder. So save the results in the parent folder: emlabpy
-    complete_path =  os.path.join(os.path.dirname(os.getcwd()),  globalNames.years_path)
-    with open(complete_path, 'w') as csvfile:
-        csvwriter = csv.writer(csvfile, delimiter ='/')
+    complete_path = os.path.join(os.path.dirname(os.getcwd()), globalNames.years_path)
+    with open(complete_path, "w") as csvfile:
+        csvwriter = csv.writer(csvfile, delimiter="/")
         csvwriter.writerow([current_year, (current_year + lookAhead), final_year])
 
 
-try:
+def main(clock_mode: str) -> int:
     class_name = "Configuration"
-    object_name = 'SimulationYears'
-    object_parameter_value_name = 'SimulationTick'
+    object_name = "SimulationYears"
+    object_parameter_value_name = "SimulationTick"
 
-    if len(sys.argv) >= 2:
-        lookAhead = next(int(i['parameter_value']) for i
-                         in db_emlab.query_object_parameter_values_by_object_class_and_object_name(class_name, object_name ) \
-                         if i['parameter_name'] == "Look Ahead")
-        final_year = next(int(i['parameter_value']) for i
-                          in db_emlab.query_object_parameter_values_by_object_class_and_object_name(class_name, object_name ) \
-                          if i['parameter_name'] == "End Year")
-        reset_candidate_investable_status()
-        print("reset power plants status")
-        if sys.argv[2] == 'initialize_clock':
-            print('Initializing clock (tick 0)')
-            db_emlab.import_object_classes([class_name])
-            db_emlab.import_objects([(class_name, object_name)])
-            db_emlab.import_data({'object_parameters': [[class_name, object_parameter_value_name]]})
-            db_emlab.import_alternatives([str(0)])
-            db_emlab.import_object_parameter_values([(class_name, object_name, object_parameter_value_name, 0, '0')])
-            StartYear = next(int(i['parameter_value']) for i in db_emlab.query_object_parameter_values_by_object_class_and_object_name(class_name, object_name) \
-                             if i['parameter_name'] == 'Start Year')
-            db_emlab.import_object_parameter_values([(class_name, object_name, "CurrentYear", StartYear, '0')])
-            db_emlab.import_object_parameter_values([(class_name, object_name, object_parameter_value_name, 0, '0')])
+    look_ahead = next(
+        int(i["parameter_value"])
+        for i in db.query_object_parameter_values_by_object_class_and_object_name(class_name, object_name)
+        if i["parameter_name"] == "Look Ahead"
+    )
+    final_year = next(
+        int(i["parameter_value"])
+        for i in db.query_object_parameter_values_by_object_class_and_object_name(class_name, object_name)
+        if i["parameter_name"] == "End Year"
+    )
+    reset_candidate_investable_status()
+    print("reset power plants status")
+    if clock_mode == "initialize_clock":
+        print("Initializing clock (tick 0)")
+        db.import_object_classes([class_name])
+        db.import_objects([(class_name, object_name)])
+        db.import_data({"object_parameters": [[class_name, object_parameter_value_name]]})
+        db.import_alternatives([str(0)])
+        db.import_object_parameter_values([(class_name, object_name, object_parameter_value_name, 0, "0")])
+        start_year = next(
+            int(i["parameter_value"])
+            for i in db.query_object_parameter_values_by_object_class_and_object_name(class_name, object_name)
+            if i["parameter_name"] == "Start Year"
+        )
+        db.import_object_parameter_values([(class_name, object_name, "CurrentYear", start_year, "0")])
+        db.import_object_parameter_values([(class_name, object_name, object_parameter_value_name, 0, "0")])
 
-            update_years_file(StartYear , lookAhead, final_year)
-            db_emlab.commit('Clock intialization')
-            print('Done initializing clock (tick 0)')
+        update_years_file(start_year, look_ahead, final_year)
+        db.commit("Clock intialization")
+        print("Done initializing clock (tick 0)")
+    elif clock_mode == "increment_clock":
+        step = next(
+            int(i["parameter_value"])
+            for i in db.query_object_parameter_values_by_object_class_and_object_name(class_name, object_name)
+            if i["parameter_name"] == "Time Step"
+        )
 
-        if sys.argv[2] == 'increment_clock':
+        previous_tick = next(
+            int(i["parameter_value"])
+            for i in db.query_object_parameter_values_by_object_class_and_object_name(class_name, object_name)
+            if i["parameter_name"] == object_parameter_value_name
+        )
 
-            step = next(int(i['parameter_value']) for i
-                        in db_emlab.query_object_parameter_values_by_object_class_and_object_name(class_name, object_name) \
-                        if i['parameter_name'] == 'Time Step')
+        new_tick = step + previous_tick
+        print("Incrementing Clock to " + str(new_tick))
 
-            previous_tick = next(int(i['parameter_value']) for i
-                                 in db_emlab.query_object_parameter_values_by_object_class_and_object_name(class_name, object_name) \
-                                 if i['parameter_name'] == object_parameter_value_name)
-
-            new_tick = step + previous_tick
-            print('Incrementing Clock to ' + str(new_tick))
-
-            Current_year = next(int(i['parameter_value']) for i in db_emlab.query_object_parameter_values_by_object_class_and_object_name(class_name, object_name) \
-                                if i['parameter_name'] == 'CurrentYear')
-            updated_year = step + Current_year
-            db_emlab.import_object_parameter_values([(class_name, object_name, object_parameter_value_name, new_tick, '0')])
-            db_emlab.import_object_parameter_values([(class_name, object_name, "CurrentYear", updated_year, '0')])
-            update_years_file(updated_year , lookAhead, final_year)
-            db_emlab.commit('Clock increment')
-            print('Done incrementing clock (tick +' + str(step) + '), resetting invest file and years file')
-
+        current_year = next(
+            int(i["parameter_value"])
+            for i in db.query_object_parameter_values_by_object_class_and_object_name(class_name, object_name)
+            if i["parameter_name"] == "CurrentYear"
+        )
+        updated_year = step + current_year
+        db.import_object_parameter_values([(class_name, object_name, object_parameter_value_name, new_tick, "0")])
+        db.import_object_parameter_values([(class_name, object_name, "CurrentYear", updated_year, "0")])
+        update_years_file(updated_year, look_ahead, final_year)
+        db.commit("Clock increment")
+        print("Done incrementing clock (tick +" + str(step) + "), resetting invest file and years file")
     else:
-        print('No mode specified.')
-except Exception:
-    raise
-finally:
-    print('Closing DB Connections...')
-    db_emlab.close_connection()
+        print(f"Mode not implemented: {clock_mode}")
+        return 1
+    return 0
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Needs exactly two arguments: 'database_url' and 'mode'")
+        sys.exit(1)
+
+    db_url = sys.argv[1]
+    print(f"Connecting to DB {db_url}")
+    db = SpineDB(db_url)
+    mode = sys.argv[2]
+
+    try:
+        exit_code = main(mode)
+        if exit_code != 0:
+            print(f"Clock.py failed with exit code: {exit_code}")
+    finally:
+        print("Closing DB Connections...")
+        db.close_connection()
