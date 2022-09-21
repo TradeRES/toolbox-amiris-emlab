fefc9e604a797b2077ad371aac666b4cd83d4f12
IngridJSJ <jeenesan@gmail.com>
2022-09-21 15:50:52 +0200

    deleting unnecessary modules

--- emlabpy/testEMLABPY.py
+++ emlabpy/testEMLABPY.py
@@ -6,7 +6,13 @@ import sys
 from functools import reduce
 import pandas as pd
 from matplotlib.offsetbox import AnchoredText
-
+# start = 117000 *256
+# age = 26
+# lifetime =25
+# growth_rate = 0.001
+# after_life_growth_rate = 0.05
+# after_life = pow(1 + growth_rate, lifetime) * start
+# print(after_life)
 # data = [(1,'sravan'),(2,'ojaswi'),
 #         (3,'bobby'),(4,'rohith'),
 #         (5,'gnanesh')]
@@ -26,13 +32,13 @@ from matplotlib.offsetbox import AnchoredText
 #
 #
 # df.index.name = "key"
-df = pd.DataFrame({"A": [5, 3, None, 4],
-                   "B": [None, 2, 4, 3],
-                   "C": [4, 3, 8, 5],
-                   "D": [5, 4, 2, None]})
-aaa = pd.Series([3,4], index=[3 ,4 ])
-df["E" ] = aaa
-print(df)
+# df = pd.DataFrame({"A": [5, 3, None, 4],
+#                    "B": [None, 2, 4, 3],
+#                    "C": [4, 3, 8, 5],
+#                    "D": [5, 4, 2, None]})
+# aaa = pd.Series([3,4], index=[3 ,4 ])
+# df["E" ] = aaa
+# print(df)
 # other = pd.DataFrame({'key': [1, 2, 5],
 #                       'B': ['B0', 'B1', 'B2']})
 # ssd = pd.DataFrame({'key': [1, 2, 3],
@@ -47,38 +53,36 @@ print(df)
 # merged_df = reduce(lambda df, other: pd.merge(df, other, on='date', how='inner'), dfs)
 #
 #
-# record1 = {'Math': list(range(100))}
-#
-# record2 = {'Math': [0, 0, 0, 0, 0],
-#            'Science': [0, 0, 0, 0, 0],
-#            'English': [0, 0, 0, 0, 100000]}
-#
-#
-# def plot_investments_per_iteration(candidate_plants_project_value, installed_capacity_per_iteration,
-#                                    ):
-#     print('project values')
-#     fig8, ax1 = plt.subplots()
-#     ax2 = ax1.twinx()
-#     ax1.annotate('figure fraction',
-#                  xy=(.025, 1), xycoords='figure fraction',
-#                  horizontalalignment='left', verticalalignment='top',
-#                  fontsize='medium')
-#
-#     ax1.plot(candidate_plants_project_value)
-#     ax2.plot(installed_capacity_per_iteration, 'o')
-#     ax1.set_xlabel('Iterations', fontsize='medium')
-#     ax1.set_ylabel('Project value', fontsize='medium')
-#     ax2.set_ylabel('Investments', fontsize='medium')
-#     ax1.set_title('Investments and project value per iterations')
-#     ax1.legend(candidate_plants_project_value.columns.values.tolist())
-#     print(candidate_plants_project_value.columns)
-#
-#
-# candidate_plants_project_value = pd.DataFrame(record1)
-# installed_capacity_per_iteration = pd.DataFrame(record2)
-# plot_investments_per_iteration(candidate_plants_project_value, installed_capacity_per_iteration)
-# print('Showing plots...')
-# plt.show()
+record1 = {'Math': list(range(100))}
+
+record2 = {'Math': [0, 0, 0, 0, 0],
+           'Science': [0, 0, 0, 0, 0],
+           'English': [0, 0, 0, 0, 100000]}
+def plot_investments_per_iteration(candidate_plants_project_value, installed_capacity_per_iteration,
+                                   ):
+    print('project values')
+    fig8, ax1 = plt.subplots()
+    ax2 = ax1.twinx()
+    ax1.annotate('figure fraction',
+                 xy=(.025, 1), xycoords='figure fraction',
+                 horizontalalignment='left', verticalalignment='top',
+                 fontsize='medium')
+
+    ax1.plot(candidate_plants_project_value)
+    ax2.plot(installed_capacity_per_iteration, 'o')
+    ax1.set_xlabel('Iterations', fontsize='medium')
+    ax1.set_ylabel('Project value', fontsize='medium')
+    ax2.set_ylabel('Investments', fontsize='medium')
+    ax2.set_title('Expected future operational (wholesale market) profits \n in year')
+    ax1.legend(candidate_plants_project_value.columns.values.tolist(), fontsize='medium', loc='upper left', bbox_to_anchor=(1, 0.9))
+    print(candidate_plants_project_value.columns)
+
+
+candidate_plants_project_value = pd.DataFrame(record1)
+installed_capacity_per_iteration = pd.DataFrame(record2)
+plot_investments_per_iteration(candidate_plants_project_value, installed_capacity_per_iteration)
+print('Showing plots...')
+plt.show()
 
 # from domain.import_object import *
 # from twine.repository import Repository
