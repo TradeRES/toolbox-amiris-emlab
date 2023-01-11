0501ec6597c494466df1bf1a33dc53af3849127e
IngridJSJ <jeenesan@gmail.com>
2023-01-11 17:43:49 +0100

    changing order of targeted investments

--- emlabpy/util/spinedb_reader_writer.py
+++ emlabpy/util/spinedb_reader_writer.py
@@ -124,6 +124,8 @@ class SpineDBReaderWriter:
                 reps.Power_plants_from_year = int(row['parameter_value'])
             elif row['parameter_name'] == 'install_at_look_ahead_year':
                 reps.install_at_look_ahead_year = bool(row['parameter_value'])
+            elif row['parameter_name'] == 'market_driven_renewables_investments_done':
+                reps.market_driven_renewables_investments_done = bool(row['parameter_value'])
 
         # these are the years that need to be added to the power plants on the first simulation tick
         reps.add_initial_age_years = reps.start_simulation_year - reps.Power_plants_from_year
@@ -590,6 +592,10 @@ class SpineDBReaderWriter:
     def stage_init_future_prices_structure(self):
         self.stage_object_class(self.fuel_classname)
         self.stage_object_parameters(self.fuel_classname, [globalNames.future_prices])
+
+    def stage_market_driven_RE_investments_done(self):
+        self.stage_object_parameter_values(self.configuration_object_classname, "SimulationYears",
+                                           [("market_driven_renewables_investments_done", True)], "0")
 
     def stage_simulated_fuel_prices(self, year, price, substance):
         object_name = substance.name
