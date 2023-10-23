a29a1ec3953663f604fff227cb5055f798e21164
Ingrid Sanchez Jimenez <47012366+IngridJSJ@users.noreply.github.com>
2023-10-23 11:49:11 +0200

    cleaning DB from batch file

--- ______update_emlabpy_SPINETOOLBOX.bat
+++ ______update_emlabpy_SPINETOOLBOX.bat
@@ -3,5 +3,6 @@ python setup.py install
 python -m pip install --editable .
 call conda deactivate
 call activate spinetoolbox
+python "C:\toolbox-amiris-emlab\scripts\Util\CleanDBs.py"
 spinetoolbox
 @pause
