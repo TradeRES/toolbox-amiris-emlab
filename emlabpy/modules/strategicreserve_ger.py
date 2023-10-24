f8ebb14dd6ad6451ae946bb5e8c4a6858c1c5ad2
Ingrid Sanchez Jimenez <47012366+IngridJSJ@users.noreply.github.com>
2023-10-24 16:47:16 +0200

    update SR ger

--- emlabpy/modules/strategicreserve_ger.py
+++ emlabpy/modules/strategicreserve_ger.py
@@ -27,12 +27,7 @@ class StrategicReserveSubmitBids_ger(MarketModule):
             # Retrieve the active capacity market and power plant capacity
             market = self.reps.get_capacity_market_for_plant(powerplant)
             power_plant_capacity = powerplant.get_actual_nominal_capacity()
-            variable_costs = powerplant.technology.get_variable_operating_by_time_series(
-                powerplant.age + self.operator.forward_years_SR)
-
-            Bid = variable_costs + powerplant.technology.fuel.futurePrice[
-                self.operator.forward_years_SR + self.reps.current_year] / powerplant.technology.get_efficiency_by_time_series(
-                powerplant.age + self.operator.forward_years_SR)
+            Bid  = self.reps.calculate_marginal_costs( powerplant, self.operator.forward_years_SR)
             # Bid = powerplant.getActualFixedOperatingCost()
             # if powerplant.age < powerplant.technology.expected_lifetime:
             #     Bid += powerplant.getLoan().getAmountPerPayment()  # if power plant is reaches its lifetime it should not have anymore payments left
@@ -60,7 +55,6 @@ class StrategicReserveAssignment_ger(MarketModule):
     def act(self):
         # Retrieve the active capacity market
         market = self.reps.get_capacity_market_in_country(self.reps.country)
-
         # Retrieve the active strategic reserve operator in the country
         self.operator = self.reps.get_strategic_reserve_operator(self.reps.country)
 
@@ -130,7 +124,7 @@ class StrategicReserveAssignment_ger(MarketModule):
                     list_of_plants.append(ppdp.plant)
                     # Change plant status and increase age
                     power_plant = self.reps.get_power_plant_by_name(ppdp.plant)
-                    print("new in Reserve"  + power_plant.name)
+                    print("new in Reserve "  + power_plant.name)
                     self.reps.update_power_plant_status_ger_first_year(power_plant)
                     contracted_strategic_reserve_capacity += ppdp.amount
                     if (contracted_strategic_reserve_capacity) > strategic_reserve_capacity:
@@ -146,49 +140,52 @@ class StrategicReserveAssignment_ger(MarketModule):
         # Pass the total contracted volume to the strategic reserve operator
         self.operator.setReserveVolume(contracted_strategic_reserve_capacity)
 
-        # Pay the contracted plants in the strategic reserve and save the revenues to the power plants
-        self.createCashFlowforSR(market)
-
-        # Save the SR operator variables to the SR operator of the country
         self.reps.create_or_update_StrategicReserveOperator(self.operator.name,
                                                             self.operator.getZone(),
                                                             self.operator.getReserveVolume(),
-                                                            self.operator.getCash(),
-                                                            self.operator.revenues_per_year,
                                                             self.operator.getPlants())
 
+
+
+
     # Cashflow function for the operation of the strategic reserve
-    def createCashFlowforSR(self, market):
-        accepted_ppdp = self.reps.get_accepted_SR_bids()
-        for accepted in accepted_ppdp:
-            plant = self.reps.power_plants[accepted.plant]
-            print("-------------------------------------------------------------" + str(plant.name))
-            # Fixed operating costs of plants
-            fixed_operating_costs = plant.actualFixedOperatingCost
-            # Retrieve dispatch data of plants for variable costs and revenues
-            dispatch = self.reps.get_power_plant_electricity_dispatch(plant.id)
-            # Costs to be paid by Strategic Reserve Operator and to be received
-            if dispatch is None:
-                SR_payment_to_plant = fixed_operating_costs
-                SR_payment_to_operator = 0
-            else:
-                SR_payment_to_plant = fixed_operating_costs + dispatch.variable_costs
-                SR_payment_to_operator = dispatch.revenues
+    def createCashFlowforSR(self, plant, operator):
+        """ Pay the contracted plants in the strategic reserve and save the revenues to the power plants
+        """
+        print("-----------------SR plant" + str(plant.name))
+        # Fixed operating costs of plants
+        fixed_operating_costs = plant.actualFixedOperatingCost
+        # Retrieve dispatch data of plants for variable costs and revenues
+        dispatch = self.reps.get_power_plant_electricity_dispatch(plant.id)
+        # Costs to be paid by Strategic Reserve Operator and to be received
+        if dispatch is None:
+            SR_payment_to_plant = fixed_operating_costs
+            SR_payment_to_operator = 0
+            real_commodity_costs = 0
+        else:
+            marginal_costs =  self.reps.calculate_marginal_costs( plant, 0)
+            real_commodity_costs =  dispatch.accepted_amount * marginal_costs
+            SR_payment_to_plant = fixed_operating_costs + real_commodity_costs
+            SR_payment_to_operator = dispatch.revenues - real_commodity_costs
 
-            if plant.loan.getNumberOfPaymentsDone() < plant.loan.getTotalNumberOfPayments(): # that year the plant should still pay the loans
-                print("plus loans" + str(plant.loan.getAmountPerPayment()))
-                SR_payment_to_plant = SR_payment_to_plant +  plant.loan.getAmountPerPayment()
-            print("SR_payment_to_plant" + str(SR_payment_to_plant))
-            # Payment (fixed costs and variable costs ) from operator to plant
-            self.reps.createCashFlow(self.operator, plant,
-                                     SR_payment_to_plant, globalNames.CF_STRRESPAYMENT, self.reps.current_tick,
-                                     self.reps.power_plants[accepted.plant])
-            # saving the revenues to the power plants
-            self.reps.dbrw.stage_CM_revenues(accepted.plant, SR_payment_to_plant, self.reps.current_tick)
-            plant.crm_payments_in_year += SR_payment_to_plant
-            # Payment (market revenues) from market to operator
-            self.reps.createCashFlow(market, self.operator,
-                                     SR_payment_to_operator, globalNames.CF_STRRESPAYMENT, self.reps.current_tick,
-                                     self.reps.power_plants[accepted.plant])
+        if (plant.loan_payments_in_year + plant.downpayment_in_year) > 0: # that year the plant should still pay the loans
+            print("plus loans" + str(plant.loan.getAmountPerPayment()))
+            SR_payment_to_plant = SR_payment_to_plant +  plant.loan.getAmountPerPayment()
 
-            self.operator.revenues_per_year += SR_payment_to_operator
+        print("SR_payment_to_plant" + str(SR_payment_to_plant))
+
+        # saving the revenues to the power plants
+        self.reps.dbrw.stage_CM_revenues(plant.name, SR_payment_to_plant, self.reps.current_tick)
+        plant.crm_payments_in_year += SR_payment_to_plant
+
+
+        #Payment (fixed costs and variable costs ) from operator to plant
+        self.reps.createCashFlow(operator, plant,
+                                 SR_payment_to_plant, globalNames.CF_STRRESPAYMENT, self.reps.current_tick, plant)
+        #Payment (market revenues) from wholesale market to operator
+        market = self.reps.get_spot_market_in_country(self.reps.country)
+
+        operator.revenues_per_year += SR_payment_to_operator
+        self.reps.createCashFlow(market, operator,
+                                 SR_payment_to_operator, globalNames.CF_STRRESPAYMENT, self.reps.current_tick, plant)
+        return real_commodity_costs
